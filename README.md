c7568641b1d7818b9ce1d8c00a1f7a51cc05b2a1
Martin C. Doege <mdoege@compuserve.com>
2020-11-09 12:53:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## A triangular ("christmas tree"-style) 12-hour clock in Nim using SDL2
+
+Compile with
+
+```nim c christmas```
+
+The bottom triangle is a triangular hour/minute clock as proposed by [Jörg Pretz](https://arxiv.org/pdf/1006.1373.pdf) in 2010: the five lower rows (from top to bottom) are in units of 6 h, 2 h, 30 m, 6 m, and 1m, respectively.
+
+The upper triangle indicates seconds: First, there is a yellow seconds indicator as on the Berlin clock (blinks yellow when seconds are even), and the three following rows are in units of 20 s, 5 s, and 1 s, respectively.
+
+At 12 am and 12 pm, the hours/minutes indicator at the bottom goes dark. So 12 o'clock is depicted as 0 hours.
+
+Window size is fixed at 900x500 pixels.
+
+The time in the screenshot below is 10:28:10:
+
+![screenshot](https://github.com/mdoege/nim-christmas/raw/master/screenshot.png "nim-christmas screenshot")
---
+++ christmas.nim
@@ -0,0 +1,76 @@
+## A triangular ("christmas tree"-style) 12-hour clock in Nim using SDL2
+
+import sdl2, times, os
+
+discard sdl2.init(INIT_EVERYTHING)
+
+var
+  window: WindowPtr
+  render: RendererPtr
+
+window = createWindow("christmas", 100, 100, 500, 900, SDL_WINDOW_SHOWN)
+render = createRenderer(window, -1, Renderer_Accelerated or
+    Renderer_PresentVsync or Renderer_TargetTexture)
+
+var
+  evt = sdl2.defaultEvent
+  runGame = true
+  r: Rect
+
+r.w = 80
+r.h = 80
+
+let tree = [1, 2, 3, 4, 1, 2, 3, 4, 5]
+
+while runGame:
+  let d = now()
+  var
+    h = d.hour mod 12
+    m = d.minute
+    s = d.second
+    s1, s5, s20: int
+    h6, h2, m30, m6, m1, mm: int
+  let secblink = (s + 1) mod 2
+  s20 = s div 20
+  s -= s20 * 20
+  s5 = s div 5
+  s1 = s mod 5
+  h6 = h div 6
+  h -= h6 * 6
+  h2 = h div 2
+  h -= h2 * 2
+  mm = 60 * h + m
+  m30 = mm div 30
+  mm -= m30 * 30
+  m6 = mm div 6
+  m1 = mm mod 6
+
+  while pollEvent(evt):
+    if evt.kind == QuitEvent:
+      runGame = false
+      break
+  render.setDrawColor(0, 0, 0, 255)
+  render.clear
+
+  let on = [secblink, s20, s5, s1, h6, h2, m30, m6, m1]
+  for y in 0..8:
+    for x in 0..5:
+      if x >= tree[y]:
+        continue
+      if x < on[y]:
+        if y == 0:
+          render.setDrawColor(170, 170, 85, 255)
+        else:
+          render.setDrawColor(0, 170, 85, 255)
+      else:
+        render.setDrawColor(0, 82, 41, 255)
+      r.x = 10 + 100 * cint(x) + (250 - 50 * cint(tree[y]))
+      r.y = 10 + 100 * cint(y)
+      render.fillRect(r)
+
+  render.present
+  sleep(100)
+
+destroy render
+destroy window
+
